f1cc19e8da4fa6bd65d4e1551e8b9dd1d52a3e3a
mtd8 <158385079+mtd8@users.noreply.github.com>
2025-01-29 14:56:46 -0600

    Added LCD code

--- test2/test2.ino
+++ test2/test2.ino
@@ -1,11 +1,14 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+  lcd.begin(16, 2); //Initiate the LCD in a 16x2 configuration
+  lcd.print("My name is:")
+  lcd.setCursor(0, 1);
+  lcd.print("[Your name here!]")
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50) //Wait 50 ms
 }
